abb559ce4f370d382127f0422afa2067cde5b927
Rohit Mighwal <rkmighwal@gmail.com>
2023-10-02 11:07:08 +0200

    added extra files

--- .github/actions/deploy-s3-js/main.js
+++
@@ -1,9 +0,0 @@
-const github = require('@actions/github');
-const core = require('@actions/core');
-const exec = require('@actions/exec');
-
-function run() {
-    core.notice('Hello for JS custom action!');
-};
-
-run();
