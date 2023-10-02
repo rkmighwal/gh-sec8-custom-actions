95697d3605bdf3c356d4122432e5dded0add995a
Rohit Mighwal <rkmighwal@gmail.com>
2023-10-02 12:13:24 +0200

    updated s3 custom action

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,9 +1,25 @@
-const github = require('@actions/github');
-const core = require('@actions/core');
-const exec = require('@actions/exec');
+// const github = require("@actions/github");
+const core = require("@actions/core");
+const exec = require("@actions/exec");
 
 function run() {
-    core.notice('Hello for JS custom action!');
-};
+  const bucket = core.getInput("bucket", {
+    required: true,
+    trimWhitespace: true,
+  });
+  const bucketRegion = core.getInput("bucket-region", {
+    required: true,
+    trimWhitespace: true,
+  });
+  const distFolder = core.getInput("dist-folder", {
+    required: true,
+    trimWhitespace: true,
+  });
+
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+  core.notice("Hello for JS custom action!");
+}
 
 run();
